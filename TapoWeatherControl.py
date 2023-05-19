78b799c9864e713a04bf7b4a689e78452653a318
Luca Di Matteo <98836568+dimatteoluca@users.noreply.github.com>
2023-05-19 23:23:07 +0200

    Update TapoWeatherControl.py

--- TapoWeatherControl.py
+++ TapoWeatherControl.py
@@ -1,17 +1,27 @@
 import logging
 import os
-from dotenv import load_dotenv      # pip install python-dotenv
-from utils import weather_functions
-from utils import tapo_functions
+import threading
+from dotenv import load_dotenv  # pip install python-dotenv
+try:
+    from utils import weather_functions
+    from utils import tapo_functions
+except ImportError:             # handle the case when the direct import fails, likely when executing through an external file
+    from .utils import weather_functions
+    from .utils import tapo_functions
+
+# Absolute path of the current folder
+folder_path = os.path.dirname(os.path.abspath(__file__))
 
 # Logger configuration
-logging.basicConfig(filename='./app.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+log_file = os.path.join(folder_path, 'app.log')
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 # Loading environment variables from the .env file
-load_dotenv("config.env")
+config_file = os.path.join(folder_path, 'config.env')
+load_dotenv(config_file)
 
 # Configuration values
-cloudiness_breakpoint = 33  # percentage
+CLOUDINESS_BREAKPOINT = 33  # percentage
 
 # Tapo's plugs and bulbs configuration
 tapo_email = os.getenv("TAPO_EMAIL")
@@ -38,26 +48,38 @@ latitude =  os.getenv("LATITUDE")
 longitude = os.getenv("LONGITUDE")
 api_key =   os.getenv("OPEN_WEATHER_API_KEY")
 
-def start():
+def start_control():
     # Get weather information
     weather_info = weather_functions.get_weather(latitude, longitude, api_key)
 
     if weather_info:
         # If the weather it's cloudy turn on the light
         cloudiness = weather_info["clouds"]["all"]
-        if cloudiness > cloudiness_breakpoint:
+        threads = []
+        if cloudiness > CLOUDINESS_BREAKPOINT:
             logging.info(f"Cloudy ({cloudiness}%)")
             for device_name, device_params in devices.items():
-                tapo_functions.if_off_turn_on(device_params)
+                try:
+                    thread = threading.Thread(target=tapo_functions.if_off_turn_on, args=([device_params]))
+                    threads.append(thread)
+                    thread.start()
+                except Exception as e:
+                    logging.error(f"Error occurred in the thread management (1): {str(e)}")
         else:
             logging.info(f"Not cloudy ({cloudiness}%)")
             for device_name, device_params in devices.items():
-                tapo_functions.if_on_turn_off(device_params)
+                try:
+                    thread = threading.Thread(target=tapo_functions.if_on_turn_off, args=([device_params]))
+                    threads.append(thread)
+                    thread.start()
+                except Exception as e:
+                    logging.error(f"Error occurred in the thread management (2): {str(e)}")
+        for thread in threads:
+            thread.join()
     else:
         logging.error("Unable to retrieve weather data.")
 
 if __name__ == "__main__":
-    start()
+    start_control()
     
 logging.info("------------------------------------------")
-
